24b03f15cecb07932b83ac52c60759b08ff9066d
Shinaikos <diguin-76@hotmail.com>
2021-02-28 21:59:29 -0300

    template finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,23 +19,28 @@ tog.addEventListener("click", function () {
 cash.addEventListener("input", function () {
     if(cash.value == "1"){
         view.innerHTML = "10k pageviews";
-        price.innerHTML = "$8.00"
         price.value = 8;
+        tog.classList.remove("toggle_select");
+        price.innerHTML = "$8.00"
     }else if(cash.value == "2"){
         view.innerHTML = "50k pageviews";
-        price.innerHTML = "$12.00"
+        tog.classList.remove("toggle_select");
         price.value = 12;
+        price.innerHTML = "$12.00"
     }else if(cash.value == "3"){
         view.innerHTML = "100k pageviews";
-        price.innerHTML = "$16.00"
+        tog.classList.remove("toggle_select");
         price.value = 16;
+        price.innerHTML = "$16.00"
     }else if(cash.value == "4"){
         view.innerHTML = "500k pageviews";
-        price.innerHTML = "$24.00"
+        tog.classList.remove("toggle_select");
         price.value = 24;
+        price.innerHTML = "$24.00"
     }else{
         view.innerHTML = "1m pageviews";
-        price.innerHTML = "$36.00"
+        tog.classList.remove("toggle_select");
         price.value = 36;
+        price.innerHTML = "$36.00"
     }
 }, false);
